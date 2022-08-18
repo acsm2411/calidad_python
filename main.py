ac53d1bdc6353930b20a6e5e9af66fa6093bfb09
Andres Silva <Andres.Silva@anuntatech.com>
2022-08-17 20:19:01 -0500

    Agregados logs y prometheus

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
 import requests
+from prometheus_fastapi_instrumentator import Instrumentator
+import logging
+
+# setup loggers
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+
+# get root logger
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -12,4 +20,7 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    # logger.info("Testing logger")
     return {"item_id": item_id, "q": q}
+
+Instrumentator().instrument(app).expose(app)
